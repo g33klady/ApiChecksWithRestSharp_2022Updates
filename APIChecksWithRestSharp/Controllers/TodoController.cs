ba25f17caa67c3c88a8c9f9ac264e1e533f110cc
Hilary Weaver-Robb <g33klady@gmail.com>
2022-02-05 12:34:04 -0500

    Updated the base solution to dotnet 6, updated swagger as well

--- APIChecksWithRestSharp/Controllers/TodoController.cs
+++ APIChecksWithRestSharp/Controllers/TodoController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using APIChecksWithRestSharp.Models;
-using Microsoft.AspNetCore.Authorization;
 
 namespace APIChecksWithRestSharp.Controllers
 {
--- APIChecksWithRestSharp/Program.cs
+++ APIChecksWithRestSharp/Program.cs
@@ -1,17 +1,72 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using APIChecksWithRestSharp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
-namespace APIChecksWithRestSharp
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    public class Program
+    c.AddSecurityDefinition("CanAccess", new OpenApiSecurityScheme
     {
-        public static void Main(string[] args)
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Name = "CanAccess"
+    });
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            Type = ReferenceType.SecurityScheme,
+            Id = "CanAccess"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { key, new List<string>() }
+    };
+    c.AddSecurityRequirement(requirement);
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
+});
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
-}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseEndpoints(opts =>
+{
+    opts.MapControllers();
+});
+
+app.UseHttpsRedirection();
+
+
+app.MapControllers();
+
+app.Run();
