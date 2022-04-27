4fc022bcd247520ef99735af491fc33c0c3b3e7b
Hilary <g33klady@gmail.com>
2022-04-27 15:50:54 -0400

    added apichecks project and first check

---
+++ ApiChecks/ApiChecksClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System.Net;
+
+namespace ApiChecks
+{
+    [TestFixture]
+    public class ApiChecksClass
+    {
+        [Test]
+        public async Task VerifyGetAllTodoItemsReturns200()
+        {
+            //Arrange
+            var client = new RestClient("https://localhost:44367/api/Todo");
+            var request = new RestRequest();
+
+            //Act
+            RestResponse response = await client.GetAsync(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"GET all todo items did not return a success status code; it returned {response.StatusCode}");
+        }
+    }
+}
